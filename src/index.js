cf4f1166e9ed3b93ae2e6c41f8c6ad1046eaf6cc
V <valen.bagaglia@gmail.com>
2023-05-29 13:49:25 +0200

    checkpoint

--- src/index.js
+++ src/index.js
@@ -3,22 +3,3 @@ console.log('yello');
 import initWebsite from './modules/initial-loadPage';
 
 initWebsite();
-
-  /*
-function addNavEvents() {
-    const homeBtn = document.getElementById('home');
-    const menuBtn = document.getElementById('menu');
-    const contactBtn = document.getElementById('contact');
-  
-    homeBtn.addEventListener('click', renderHome);
-    menuBtn.addEventListener('click', renderMenu);
-    contactBtn.addEventListener('click', renderContact);
-  };
-
-
-function initWebsite() {
-    loadPage();
-    renderHome();
-    addNavEvents();
-};
-*/
