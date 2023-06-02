d80cc87182fbaaf5bb60f43ccfb2729542c7540c
V <valen.bagaglia@gmail.com>
2023-06-02 20:50:04 +0200

    working menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -18,64 +18,68 @@ const menuItems = [
 ];
 
 function createMenu() {
-  const menuDiv = document.createElement("div");
-  menuDiv.classList.add("menu");
+  const menuDiv = document.createElement('div');
+  menuDiv.className = 'menu';
 
-  const heroDiv = document.createElement("div");
-  heroDiv.classList.add("hero");
-
-  const menuHeaderDiv = document.createElement("div");
-  menuHeaderDiv.classList.add("menu-header");
-
-  const h1 = document.createElement("h1");
-  h1.classList.add("h-large");
-  h1.textContent = "Menu";
-
-  menuHeaderDiv.appendChild(h1);
-  heroDiv.appendChild(menuHeaderDiv);
+  const heroDiv = document.createElement('div');
+  heroDiv.className = 'hero';
   menuDiv.appendChild(heroDiv);
 
-  const menuContainerDiv = document.createElement("div");
-  menuContainerDiv.classList.add("menu-container");
+  const menuHeaderDiv = document.createElement('div');
+  menuHeaderDiv.className = 'menu-header';
+  heroDiv.appendChild(menuHeaderDiv);
 
-  const mainCourseH3 = document.createElement("h3");
-  mainCourseH3.classList.add("h-medium");
-  mainCourseH3.textContent = "Main course";
+  const h1 = document.createElement('h1');
+  h1.className = 'h-large';
+  h1.textContent = 'Menu';
+  menuHeaderDiv.appendChild(h1);
 
-  menuContainerDiv.appendChild(mainCourseH3);
-
-  const mainCourseSectionDiv = document.createElement("div");
-  mainCourseSectionDiv.classList.add("menu-section");
-
-  menuItems.forEach((menuItem) => {
-    const menuItemDiv = document.createElement("div");
-    menuItemDiv.classList.add("menu-item");
-
-    const itemNameSpan = document.createElement("span");
-    itemNameSpan.classList.add("item-name");
-    itemNameSpan.textContent = menuItem.title;
-
-    const itemDecoSpan = document.createElement("span");
-    itemDecoSpan.classList.add("item-deco");
-
-    const priceSpan = document.createElement("span");
-    priceSpan.classList.add("price");
-    priceSpan.textContent = menuItem.price;
-
-    menuItemDiv.appendChild(itemNameSpan);
-    menuItemDiv.appendChild(itemDecoSpan);
-    menuItemDiv.appendChild(priceSpan);
-
-    const descriptionP = document.createElement("p");
-    descriptionP.classList.add("description");
-    descriptionP.textContent = menuItem.description;
-
-    mainCourseSectionDiv.appendChild(menuItemDiv);
-    mainCourseSectionDiv.appendChild(descriptionP);
-  });
-
-  menuContainerDiv.appendChild(mainCourseSectionDiv);
+  const menuContainerDiv = document.createElement('div');
+  menuContainerDiv.className = 'menu-container';
   menuDiv.appendChild(menuContainerDiv);
+
+  const sections = [
+    { title: 'Main course', items: menuItems.slice(0, 2) },
+    { title: 'Soups & salads', items: menuItems.slice(2, 4) },
+    { title: 'Desserts', items: menuItems.slice(4, 6) },
+    { title: 'Drinks', items: menuItems.slice(6) },
+  ];
+
+  sections.forEach((section) => {
+    const sectionHeader = document.createElement('h3');
+    sectionHeader.className = 'h-medium';
+    sectionHeader.textContent = section.title;
+    menuContainerDiv.appendChild(sectionHeader);
+
+    const menuSectionDiv = document.createElement('div');
+    menuSectionDiv.className = 'menu-section';
+    menuContainerDiv.appendChild(menuSectionDiv);
+
+    section.items.forEach((item) => {
+      const menuItemDiv = document.createElement('div');
+      menuItemDiv.className = 'menu-item';
+      menuSectionDiv.appendChild(menuItemDiv);
+
+      const itemNameSpan = document.createElement('span');
+      itemNameSpan.className = 'item-name';
+      itemNameSpan.textContent = item.title;
+      menuItemDiv.appendChild(itemNameSpan);
+
+      const itemDecoSpan = document.createElement('span');
+      itemDecoSpan.className = 'item-deco';
+      menuItemDiv.appendChild(itemDecoSpan);
+
+      const priceSpan = document.createElement('span');
+      priceSpan.className = 'price';
+      priceSpan.textContent = item.price;
+      menuItemDiv.appendChild(priceSpan);
+
+      const descriptionP = document.createElement('p');
+      descriptionP.className = 'description';
+      descriptionP.textContent = item.description;
+      menuSectionDiv.appendChild(descriptionP);
+    });
+  });
 
   return menuDiv;
 }
